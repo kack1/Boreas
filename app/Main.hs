b5f72750a56971b3da4cf9c6cbf26a6ab4916cf5
Kyle <kackerman0102@gmail.com>
2024-01-22 15:53:35 -0600

    Introducting Shell component

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,8 @@ module Main where
 import Boreas_Network
 import Boreas_Util
 import Parse
-import System.Command
 import System.Environment
+import System.Process
 
 main :: IO ()
 main = do
@@ -14,4 +14,9 @@ main = do
   x <- sequence $ getGithubKeys s
   putStrLn . unlines $
     map (\z -> show $ uncurry insertKeys z) $ zip s $ filterOK x
-  -- putStrLn . unlines $ map snd x
+
+createUserAccount :: StudentInfo -> IO ()
+createUserAccount user = callCommand $ "useradd -m " ++ universityID user
+
+deleteUserAccount :: StudentInfo -> IO ()
+deleteUserAccount user = callCommand $ "userdel -r " ++ universityID user
