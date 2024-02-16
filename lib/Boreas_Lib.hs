0a169d5f75e0d58cca37f251826b31ed7804df63
Kyle Ackerman <108965613+kack1@users.noreply.github.com>
2024-02-16 14:45:51 -0600

    Adding Haddock Documentation.

--- lib/Boreas_Lib.hs
+++ lib/Boreas_Lib.hs
@@ -1,12 +1,25 @@
--- Copyright (c) 2024, Kyle Ackerman
+{-|
+Module      : Boreas_Lib
+Description : Top-level bindings for Boreas
+Copyright   : (c) 2024, Kyle Ackerman 
+License     : BSD-2
+Maintainer  : sample@email.com
+Stability   : experimental
+Portability : POSIX
+
+                   Top-level bindings for Boreas. Contains main cmd function.
+                   such as, cmdRun, cmdUpdate, cmdPurge.
+-}
 module Boreas_Lib where
 
 import Boreas_Network
 import Boreas_Util
 import Parse
 
+-- | Config file location 
 type Config = String
 
+-- | Top level cmd call to update update accounts
 cmdUpdate :: String -> IO ()
 cmdUpdate config = do
   stds <- getStudents config
@@ -14,6 +27,7 @@ cmdUpdate config = do
   mapM_ writeKeys stds
   print stds
 
+-- | Top level cmd call to run Boreas
 cmdRun :: String -> IO ()
 cmdRun config = do
   stds <- getStudents config
@@ -22,11 +36,17 @@ cmdRun config = do
   mapM_ writeKeys stds
   print stds
 
+-- | Top level cmd call to purge/remove accounts
 cmdPurge :: String -> IO ()
 cmdPurge config = do
   stds <- getStudents config
   mapM_ deleteUserAccount stds
 
+{-|
+    The function 'getStudents' takes in a
+    config and returns a list of StudentInfo
+    with their SSH keys populated
+-}
 getStudents :: Config -> IO [StudentInfo]
 getStudents file = do
   s <- parseFile parseStudents file
--- lib/Boreas_Network.hs
+++ lib/Boreas_Network.hs
@@ -4,19 +4,24 @@ module Boreas_Network where
 import Boreas_Util
 import Network.Curl
 
-curlGithubKeys :: String -> IO (CurlCode, String)
+type Key = String
+
+-- | Takes in a GitHub username and scapes that user's keys.
+curlGithubKeys :: String -> IO (CurlCode, Key)
 curlGithubKeys gh = curlGetString userkeys []
   where
     userkeys = "https://github.com/" ++ gh ++ ".keys"
 
-getGithubKeys :: [StudentInfo] -> IO [(CurlCode, String)]
+getGithubKeys :: [StudentInfo] -> IO [(CurlCode, Key)]
 getGithubKeys stds = do
   sequence $ map (curlGithubKeys . githubID) stds
 
-insertKeys :: [StudentInfo] -> [(CurlCode, String)] -> [StudentInfo]
+insertKeys :: [StudentInfo] -> [(CurlCode, Key)] -> [StudentInfo]
 insertKeys stds ks = zipWith insertKey ks stds
 
-insertKey :: (CurlCode, String) -> StudentInfo -> StudentInfo
+-- | Inserts/Creates StudentInfo with the supplied Key and
+-- inserts it depending on the supplied CurlCode.
+insertKey :: (CurlCode, Key) -> StudentInfo -> StudentInfo
 insertKey k s =
   case k of
     (CurlOK, key) -> StudentInfo u g key
